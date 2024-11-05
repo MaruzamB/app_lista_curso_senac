a4b3030f0ce65e5df5198de89ff5b6b9e8ec0a25
MaruzamB <maruzambueno@gmail.com>
2024-11-04 21:12:27 -0300

    Criado metodo get para pegar dados

--- app/src/main/java/com/maruzamjunior/applistacurso2/view/MainActivity.java
+++ app/src/main/java/com/maruzamjunior/applistacurso2/view/MainActivity.java
@@ -14,6 +14,12 @@ import com.maruzamjunior.applistacurso2.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    Pessoa outraPessoa;
+
+    //criar os tipos de dado
+    String dadosPessoa;
+    String dadosOutraPessoa;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,12 +32,46 @@ public class MainActivity extends AppCompatActivity {
 
 
         });
+
+        //setar os dados
         pessoa = new Pessoa();
 
         pessoa.setPrimeiroNome("Maruzam");
         pessoa.setSobrenome("Bueno");
         pessoa.setCursoDesejado("Java");
         pessoa.setTelefoneContato("99999");
+
+        outraPessoa = new Pessoa();
+
+        outraPessoa.setPrimeiroNome("Darty");
+        outraPessoa.setSobrenome("Vader");
+        outraPessoa.setCursoDesejado("Python");
+        outraPessoa.setTelefoneContato("88888");
+
+
+        //pegar os dados
+        dadosPessoa = "Primeiro Nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobrenome();
+        dadosPessoa += " Curso Desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone Contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        dadosOutraPessoa = "Primeiro Nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += outraPessoa.getSobrenome();
+        dadosOutraPessoa += " Curso Desejado: ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone Contato: ";
+        dadosOutraPessoa += outraPessoa.getTelefoneContato();
+
+        int parada = 0;
+
+
+
     }
 
 }
